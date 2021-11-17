1ecdc6be57879a6285c865a5ff351a275a8f6cff
AdrianLeonRivera <56595479+AdrianLeonRivera@users.noreply.github.com>
2021-11-17 09:53:55 +0100

    Subida ejercicio prometheus

---
+++ ActuatorPrometheus/src/test/java/com/bootcamp/actuatorprometheus/ActuatorPrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.actuatorprometheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorPrometheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
